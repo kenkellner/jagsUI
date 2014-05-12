ee633621a56d4aa1cb5f9cd5a30bad0c10b4f8df
Ken Kellner <ken.kellner@gmail.com>
2014-05-11 22:00:35 -0400

    Add ability to plot specific parameters in traceplot().

--- R/traceplot.R
+++ R/traceplot.R
@@ -12,35 +12,37 @@ setGeneric("traceplot",
 setClass("simplejags")
 
 setMethod("traceplot", signature(x = "simplejags"),
-  function (x, ...) {
+  function (x, parameters=NULL, ...) {
     if(class(x)!="simplejags"){stop('Requires simplejags object as input')}
-      samples <- x$samples
-      params <- names(as.data.frame(samples[[1]]))
-      nparams <- length(params)
-      nchains <- x$mcmc.info[[1]]
-      rhat <- x$Rhat
+    samples <- x$samples
+    
+    if(is.null(parameters)){params <- names(as.data.frame(samples[[1]]))
+    } else {params <- translate.params(x,parameters)}
+    nparams <- length(params)
+    nchains <- x$mcmc.info[[1]]
+    rhat <- x$Rhat
             
-      sep <- par()$ask
-      par(ask=TRUE)
+    sep <- par()$ask
+    par(ask=TRUE)
             
-      xmax <- x$mcmc.info[[6]] / nchains
+    xmax <- x$mcmc.info[[6]] / nchains
             
-      col=c('red','blue','green','yellow','orange','violet')
-      if(nchains>6){col=rainbow(nchains)}
+    col=c('red','blue','green','yellow','orange','violet')
+    if(nchains>6){col=rainbow(nchains)}
             
-      for (i in 1:nparams){
+    for (i in 1:nparams){
+      if(nchains>1){
+        rhat <- gelman.diag(samples[,i])$psrf[1]
+        title <- paste('Trace of ',params[i],', Rhat = ',round(rhat,2),sep="")
+      } else {title <- paste('Trace of ',params[i],sep="") }
+        plot(x = 1:xmax, y = samples[,i][[1]], main = title, xlab="Iterations", ylab="Value",type="l", col=col[1],
+            ylim=range(samples[,i]))
         if(nchains>1){
-          rhat <- gelman.diag(samples[,i])$psrf[1]
-          title <- paste('Trace of ',params[i],', Rhat = ',round(rhat,2),sep="")
-        } else {title <- paste('Trace of ',params[i],sep="") }
-          plot(x = 1:xmax, y = samples[,i][[1]], main = title, xlab="Iterations", ylab="Value",type="l", col=col[1],
-               ylim=range(samples[,i]))
-          if(nchains>1){
-            for (j in 2:nchains){
-              lines(x = 1:xmax, y = samples[,i][[j]],type="l", col=col[j])
-          }}
-        }
+          for (j in 2:nchains){
+            lines(x = 1:xmax, y = samples[,i][[j]],type="l", col=col[j])
+        }}
+      }
             
-        on.exit(par(ask=sep))            
+    on.exit(par(ask=sep))            
   }
 )
