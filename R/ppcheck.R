7e32a53a89710f3d3007aaeb33197d80d01ab210
Ken Kellner <ken@kenkellner.com>
2019-02-15 09:45:56 -0500

    Improve pp.check function

--- R/ppcheck.R
+++ R/ppcheck.R
@@ -1,14 +1,29 @@
 
-pp.check <- function(x, actual, new){
+pp.check <- function(x, observed, simulated, xlab=NULL, ylab=NULL, main=NULL){
   if(class(x)!="jagsUI"){stop('Requires jagsUI object as input')}
   devAskNewPage(ask=FALSE)
-  actual <- eval(parse(text=paste('x$sims.list$',actual,sep="")))
-  new <- eval(parse(text=paste('x$sims.list$',new,sep="")))
+  observed <- eval(parse(text=paste('x$sims.list$',observed,sep="")))
+  simulated <- eval(parse(text=paste('x$sims.list$',simulated,sep="")))
   
-  bpval <- mean(actual>new)
+  bpval <- mean(simulated>observed)
+
+  if(is.null(xlab)){
+    xlab <- 'Observed Data'
+  }
+  if(is.null(ylab)){
+    ylab <- 'Simulated Data'
+  }
+  if(is.null(main)){
+    main <- paste('Posterior Predictive Check','\n',
+                  'Bayesian P-value =',round(bpval,2))
+  }
+
+  minval <- min(c(observed,simulated))
+  maxval <- max(c(observed,simulated))
+  plotrange <- c(minval,maxval)
   
-  plot(x = actual, y = new, xlab="Actual Dataset", ylab="Simulated Dataset", 
-       main = paste('Posterior Predictive Check','\n','Bayesian P-value =',round(bpval,2)))
+  plot(x = observed, y = simulated, xlab=xlab, ylab=ylab, main=main, 
+       xlim=plotrange,ylim=plotrange)
   abline(1,1)
   
 
