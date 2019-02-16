d1f33ce4fa4ee1a1c6220500962abb9de7077d8e
Mike Meredith <mmeredith@wcsmsia>
2019-02-16 15:40:00 +0800

    Tweaks to pp.check

--- R/ppcheck.R
+++ R/ppcheck.R
@@ -1,11 +1,15 @@
 
-pp.check <- function(x, observed, simulated, xlab=NULL, ylab=NULL, main=NULL){
+pp.check <- function(x, observed, simulated, xlab=NULL, ylab=NULL, main=NULL, ...){
   if(class(x)!="jagsUI"){stop('Requires jagsUI object as input')}
   devAskNewPage(ask=FALSE)
-  observed <- eval(parse(text=paste('x$sims.list$',observed,sep="")))
-  simulated <- eval(parse(text=paste('x$sims.list$',simulated,sep="")))
-  
-  bpval <- mean(simulated>observed)
+  obs <- eval(parse(text=paste('x$sims.list$',observed,sep="")))
+  if(is.null(obs))
+    stop("MCMC chain ", deparse(substitute(observed)), " not found.", call.=FALSE)
+  sim <- eval(parse(text=paste('x$sims.list$',simulated,sep="")))
+  if(is.null(sim))
+    stop("MCMC chain ", deparse(substitute(siomulated)), " not found.", call.=FALSE)
+
+    bpval <- mean(sim > obs)
 
   if(is.null(xlab)){
     xlab <- 'Observed Data'
@@ -13,18 +17,26 @@ pp.check <- function(x, observed, simulated, xlab=NULL, ylab=NULL, main=NULL){
   if(is.null(ylab)){
     ylab <- 'Simulated Data'
   }
+  usermain <- main
   if(is.null(main)){
     main <- paste('Posterior Predictive Check','\n',
                   'Bayesian P-value =',round(bpval,2))
   }
 
-  minval <- min(c(observed,simulated))
-  maxval <- max(c(observed,simulated))
-  plotrange <- c(minval,maxval)
-  
-  plot(x = observed, y = simulated, xlab=xlab, ylab=ylab, main=main, 
-       xlim=plotrange,ylim=plotrange)
-  abline(1,1)
-  
+  # minval <- min(c(observed,simulated))
+  # maxval <- max(c(observed,simulated))
+  # plotrange <- c(minval,maxval)
+  plotrange <- range(obs, sim)
 
+  plot(x = obs, y = sim, xlab=xlab, ylab=ylab, main=main,
+       xlim=plotrange,ylim=plotrange, ...)
+  # abline(1,1)
+  abline(0,1, lwd=2)
+  if(!is.null(usermain)) {
+    where <- if(bpval < 0.5) 'topleft' else 'bottomright'
+    graphics::legend(where, paste("P =", round(bpval,2)), bty='n', cex=1.5)
+  }
+
+  return(bpval)
 }
+
