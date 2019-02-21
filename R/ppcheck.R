39354068f9de38c6b0614bdeab9e46babbf45dbd
Ken Kellner <ken@kenkellner.com>
2019-02-21 10:22:06 -0500

    Put pval in plot area by default

--- R/ppcheck.R
+++ R/ppcheck.R
@@ -7,7 +7,7 @@ pp.check <- function(x, observed, simulated, xlab=NULL, ylab=NULL, main=NULL, ..
     stop("MCMC chain ", deparse(substitute(observed)), " not found.", call.=FALSE)
   sim <- eval(parse(text=paste('x$sims.list$',simulated,sep="")))
   if(is.null(sim))
-    stop("MCMC chain ", deparse(substitute(siomulated)), " not found.", call.=FALSE)
+    stop("MCMC chain ", deparse(substitute(simulated)), " not found.", call.=FALSE)
 
     bpval <- mean(sim > obs)
 
@@ -17,10 +17,8 @@ pp.check <- function(x, observed, simulated, xlab=NULL, ylab=NULL, main=NULL, ..
   if(is.null(ylab)){
     ylab <- 'Simulated Data'
   }
-  usermain <- main
   if(is.null(main)){
-    main <- paste('Posterior Predictive Check','\n',
-                  'Bayesian P-value =',round(bpval,2))
+    main <- paste('Posterior Predictive Check')
   }
 
   # minval <- min(c(observed,simulated))
@@ -32,10 +30,8 @@ pp.check <- function(x, observed, simulated, xlab=NULL, ylab=NULL, main=NULL, ..
        xlim=plotrange,ylim=plotrange, ...)
   # abline(1,1)
   abline(0,1, lwd=2)
-  if(!is.null(usermain)) {
-    where <- if(bpval < 0.5) 'topleft' else 'bottomright'
-    graphics::legend(where, paste("P =", round(bpval,2)), bty='n', cex=1.5)
-  }
+  where <- if(bpval < 0.5) 'topleft' else 'bottomright'
+  graphics::legend(where, paste("P =", round(bpval,2)), bty='n', cex=1.5)
 
   return(bpval)
 }
